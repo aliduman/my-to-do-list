b477f4a6a941d181a068493527ae63a0f6f43957
aliduman <alidumanpr@hotmail.com>
2021-04-21 23:28:25 +0300

    2 files modified

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import ListItem from './listItem/listItem'
-import './style.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import ListItem from "./listItem/listItem";
+import "./style.css";
 
 class App extends Component {
   constructor() {
@@ -9,15 +9,15 @@ class App extends Component {
     this.state = {
       text: null,
       todo: [
-        { id: 0, text: 'kod yaz', status: false },
-        { id: 1, text: 'kahve iç', status: false },
-        { id: 2, text: 'yemek ye', status: false },
+        { id: 0, text: "kod yaz", status: false },
+        { id: 1, text: "kahve iç", status: false },
+        { id: 2, text: "yemek ye", status: false }
       ]
     };
   }
 
   inputHandleChange(event) {
-    this.setState({ text: event.target.value })
+    this.setState({ text: event.target.value });
   }
 
   addItem() {
@@ -25,7 +25,7 @@ class App extends Component {
       id: this.state.todo.length + 1,
       text: this.state.text,
       status: false
-    }
+    };
     let todos = this.state.todo;
     todos.push(item);
     this.setState({
@@ -38,7 +38,7 @@ class App extends Component {
     this.setState({
       todo: this.state.todo.map(item => {
         if (item.id === getReturnProps.id) {
-          item = getReturnProps
+          item = getReturnProps;
         }
         return item;
       })
@@ -47,14 +47,25 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <input type="text" id="taskinput" onChange={this.inputHandleChange.bind(this)} /> <button onClick={() => this.addItem()}>+ add</button>
+      <div className="container">
+        <input
+          type="text"
+          id="taskinput"
+          onChange={this.inputHandleChange.bind(this)}
+        />{" "}
+        <button onClick={() => this.addItem()}>+ add</button>
         <ul>
-          {this.state.todo.map((task,index) => <ListItem key={index} item={task} change={this.changeProps.bind(this)}></ListItem>)}
+          {this.state.todo.map((task, index) => (
+            <ListItem
+              key={index}
+              item={task}
+              change={this.changeProps.bind(this)}
+            />
+          ))}
         </ul>
       </div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
